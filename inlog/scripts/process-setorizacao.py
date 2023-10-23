28dae99c2e3f1ea6203cb779c0e1e311f279ae60
Pedro Florencio <pedroflorencio@Rauls-MacBook-Air.local>
2023-10-23 11:22:18 -0300

    :recycle: convertendo o formato de geometry para multipolygon

--- inlog/scripts/process-setorizacao.py
+++ inlog/scripts/process-setorizacao.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import geopandas as gpd
 from shapely.wkt import loads
+from shapely.geometry import MultiPolygon
 
 # lendo e adequando os dados
 df = pd.read_csv('data/2023_DiasColetasParcial.csv', 
@@ -21,9 +22,9 @@ df.dropna(subset=['GEOMETRY'], inplace=True)
 
 # ajustando a coluna GEOMETRY para o formato correto
 df['GEOMETRY'] = df['GEOMETRY'].apply(lambda x: x.replace(' 0',''))
-df['GEOMETRY'] = 'POLYGON (('+df.GEOMETRY+'))'
+df['GEOMETRY'] = 'POLYGON(('+df.GEOMETRY+'))'
 
-# removendo colunas descenessarias
+# removendo colunas desnecessarias
 df.drop(columns=['CO_CIRCUITO'], inplace=True)
 
 # convertendo a coluna de texto WKT em geometrias
@@ -32,5 +33,11 @@ df['GEOMETRY'] = df['GEOMETRY'].apply(loads)
 # criando um GeoDataFrame
 gdf = gpd.GeoDataFrame(df, geometry='GEOMETRY')
 
+# convertendo em MultiPolygon
+gdf['GEOMETRY'] = gdf['GEOMETRY'].apply(lambda geom: MultiPolygon([geom]))
+
 # exportando para GeoJSON
 gdf.to_file('ColetaDomiciliarSetorizacao.geojson', driver='GeoJSON', crs='EPSG:4326')
+
+#debug
+gdf.to_csv('DEBUG_ColetaDomiciliarSetorizacao.csv', index=False, encoding='utf-8')
